b7ec9a6d1bd42a4b777b1743653c83f8065ae338
user <user@users-MacBook-Air.local>
2021-04-22 00:49:42 +0600

    bug fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './assets/styles/index.scss'
 
 function App() {
   return (
-    <BrowserRouter basename='/crypto-app'>
+    <BrowserRouter basename='/crypto'>
       <Switch>
         <Route path='/' component={HomePage} exact/>
         <Route path='/sign' component={SignPage} exact/>
